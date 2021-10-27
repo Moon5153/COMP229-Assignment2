55591563283681274283ef717b2b3ad9156a5116
Najmun Nahar <nazmun.nahar089@gmail.com>
2021-10-26 23:34:10 -0400

    updated db URI to work with MongoDb Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports=
 {
-    "URI":"mongodb://localhost/business_contacts"
+    //"URI":"mongodb://localhost/business_contacts"
+    "URI":"mongodb+srv://najmun:TjIJY9V5s9v8yBqP@mongodbserver.qgidh.mongodb.net/business_contacts?retryWrites=true&w=majority"
 }
